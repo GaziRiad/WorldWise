ce759b7ecbc5a94a2b2309fba9b81fce182e5155
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-09 17:08:39 +0100

    Nested routes and index route

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,19 @@
+import { NavLink } from "react-router-dom";
 import Styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={Styles.nav}>App Navigation</nav>;
+  return (
+    <nav className={Styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/app/cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="/app/countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
